b2896a2898b6224a221bd55d03f21a08a9c3d365
transcriptiolinguae <mesusplit@gmail.com>
2026-09-09 12:47:08 +0200

    Update it.js

--- it.js
+++ it.js
@@ -770,7 +770,7 @@ if (
 
 // qq k
 
-
+/*
 
         if (lowerText[i] === 'q' && lowerText[i + 1] === 'q') { 
     if (ipa[ipaIndex] === 'ˈ' && ipa[ipaIndex + 1] === 'k' && ipa[ipaIndex + 2] === 'ː') {
@@ -791,7 +791,7 @@ if (
       }
     }
 
-    
+  */ 
 
 
 
