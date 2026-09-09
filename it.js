78363e13f2f6c6911916eacd20c68d02085a9bc0
transcriptiolinguae <mesusplit@gmail.com>
2026-09-09 12:34:15 +0200

    Update it.js

--- it.js
+++ it.js
@@ -119,7 +119,7 @@ export function applyG2PMapping(text, ipa) {
     "mm": "mː",
     "nn": "nː",   
     "pp": "pː",
-    "qq": "kː",  
+    "qq": "ˈkː",  
     "rr": "rː",
     "ss": "sː",
     "tt": "tː",
